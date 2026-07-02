a30bbc2b06d6f4d580759964f84adf76aee20cbe
yuvika chaudhary <yuvika1904@gmail.com>
2026-07-02 10:16:33 +0530

    Time: 6 ms (49.94%), Space: 45 MB (99.39%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int x = 0;
+        int y = 0;
+        for (int i = 0; i < moves.length(); i++) {
+            char move = moves.charAt(i);
+            if (move == 'U') {
+                y++;
+            } else if (move == 'D') {
+                y--;
+            } else if (move == 'L') {
+                x--;
+            } else if (move == 'R') {
+                x++;
+            }
+        }
+        return x == 0 && y == 0;
+    }
+}
