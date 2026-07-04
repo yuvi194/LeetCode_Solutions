29c46b3f04a5dca232387fad59b6e39d4074bc98
yuvika chaudhary <yuvika1904@gmail.com>
2026-07-04 22:13:21 +0530

    Time: 80 ms (5.14%), Space: 46 MB (97.82%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        if (ransomNote.length() > magazine.length()) {
+            return false;
+        }
+        int[] freq= new int[26];
+        for (char c : magazine.toCharArray()) {
+            freq[c - 'a']++;
+        }
+        for (char c : ransomNote.toCharArray()) {
+            freq[c - 'a']--;
+            if (freq[c - 'a'] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
