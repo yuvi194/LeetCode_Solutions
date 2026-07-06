51bac58107525b2ae25a348518f47f81eea64bae
yuvika chaudhary <yuvika1904@gmail.com>
2026-07-06 09:40:17 +0530

    Time: 1 ms (61.77%), Space: 43 MB (13.06%) - LeetHub

---
+++ 0856-score-of-parentheses/0856-score-of-parentheses.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int scoreOfParentheses(String s) {
+        Stack<Integer> st = new Stack<>();
+        int count = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                st.push(count);
+                count = 0; 
+            } else {
+                count = st.pop() + Math.max(2 * count, 1);
+            }
+        }
+        return count;
+    }
+}
